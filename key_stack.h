e6d9e44eea452968b051bc54e9bbc110c3e6f40c
kamih <guillaume.stordeur@gmail.com>
2024-02-29 23:41:08 -0700

    added st_key_stack_t

---
+++ key_stack.h
@@ -0,0 +1,17 @@
+// Copyright 2024 Guillaume Stordeur <guillaume.stordeur@gmail.com>
+// Copyright 2024 Matt Skalecki <ikcelaks@gmail.com>
+// Copyright 2024 QKekos <q.kekos.q@gmail.com>
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+typedef struct
+{
+	uint16_t    *buffer;
+    uint8_t     capacity;
+	uint8_t     size;	
+} st_key_stack_t;
+
+void st_key_stack_push(st_key_stack_t *s, uint16_t key);
+void st_key_stack_pop(st_key_stack_t *s);
+void st_key_stack_to_str(const st_key_stack_t *s, char *str);
