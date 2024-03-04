9eda1bb19bf6c3049bc53a442b707ad7f4d509a4
kamih <guillaume.stordeur@gmail.com>
2024-03-04 02:37:33 -0700

    added st_strcpy and st_get_seq_token_symbol

--- utils.c
+++ utils.c
@@ -16,6 +16,8 @@
 
 // TODO: define this in generated .h file
 static const char magic_chars[] = {'M', 'R'};
+static const char *st_seq_tokens[] = {"☆", "✪"};
+static const char *st_wordbreak_token = "␣";
 
 static const char unshifted_keycode_to_ascii_lut[53] PROGMEM = {
 //                                  KC_A    KC_B    KC_C    KC_D
@@ -54,6 +56,30 @@ static const char shifted_keycode_to_ascii_lut[53] PROGMEM = {
     '?'
 };
 
+////////////////////////////////////////////////////////////////////////////////
+// copies src to dest and returns pointer to the new end of dest
+// (null terminator is written at end address)
+char *st_strcpy(char *dest, const char *src)
+{
+    // st_assert(dest != 0 && src != 0);
+    while (*src) {
+        *dest++ = *src++;
+    }
+    *dest = 0;
+    return dest;
+}
+////////////////////////////////////////////////////////////////////////////////
+// if keycode is a token that can be translated back to its user symbol,
+// returns pointer to it, otherwise returns 0
+const char *st_get_seq_token_symbol(uint16_t keycode)
+{
+    if (SPECIAL_KEY_TRIECODE_0 <= keycode && keycode < SPECIAL_KEY_TRIECODE_0 + SEQUENCE_TRANSFORM_COUNT) {
+        return st_seq_tokens[keycode - SPECIAL_KEY_TRIECODE_0];        
+    } else if (keycode == KC_SPACE) {
+        return st_wordbreak_token;
+    }
+    return 0;
+}
 ////////////////////////////////////////////////////////////////////////////////
 char st_keycode_to_char(uint16_t keycode)
 {
