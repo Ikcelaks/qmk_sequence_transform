32bb5fec7e7eb98b5343e66e5d8b5a93b933d3c2
Guillaume <guillaume.stordeur@gmail.com>
2024-03-12 19:10:54 -0600

    Added sequence transform rules.mk that should be included in user layout's rules.mk (instead of including our src files directly). This file should be updated when we add/remove .c files to the lib. (#46)

---
+++ rules.mk
@@ -0,0 +1,7 @@
+# This file should be included in user layout's rules.mk file
+LIB_SRC += sequence_transform/sequence_transform.c
+LIB_SRC += sequence_transform/utils.c
+LIB_SRC += sequence_transform/trie.c
+LIB_SRC += sequence_transform/keybuffer.c
+LIB_SRC += sequence_transform/key_stack.c
+LIB_SRC += sequence_transform/cursor.c
