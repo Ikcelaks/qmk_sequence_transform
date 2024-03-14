b1783e65766b399d6f87e7dd532ebadd2a40d870
kamih <guillaume.stordeur@gmail.com>
2024-03-13 19:36:59 -0600

    adapted test_cursor to use new RES_FAIL macro

--- tester/test_cursor.c
+++ tester/test_cursor.c
@@ -13,30 +13,23 @@
 //////////////////////////////////////////////////////////////////////
 void test_cursor(const st_test_rule_t *rule, st_test_result_t *res)
 {
-    static char message[512];
-    res->message = message;
     st_cursor_t *cursor = st_get_cursor();
     st_cursor_init(cursor, 0, false);
     for (int i = 0; i < 200; ++i) {
         st_cursor_next(cursor);
     }
-    res->pass = cursor->cursor_pos.index == cursor->buffer->context_len;
-    if (!res->pass) {
-        snprintf(message, sizeof(message), "input cursor didn't stop at end: cursor index %d; context_len: %d",
-                    cursor->cursor_pos.index,
-                    cursor->buffer->context_len);
+    if (cursor->cursor_pos.index != cursor->buffer->context_len) {
+        RES_FAIL("input cursor didn't stop at end: cursor index %d; context_len: %d",
+                 cursor->cursor_pos.index, cursor->buffer->context_len);
+        return;
     }
     if (st_cursor_init(cursor, 0, true)) {
         for (int i = 0; i < 200; ++i) {
             st_cursor_next(cursor);
         }
     }
-    res->pass = cursor->cursor_pos.index == cursor->buffer->context_len;
-    if (res->pass) {
-        snprintf(message, sizeof(message), "OK!");
-    } else {
-        snprintf(message, sizeof(message), "output cursor didn't stop at end: cursor index %d; context_len: %d",
-                    cursor->cursor_pos.index,
-                    cursor->buffer->context_len);
+    if (cursor->cursor_pos.index != cursor->buffer->context_len) {
+        RES_FAIL("output cursor didn't stop at end: cursor index %d; context_len: %d",
+                 cursor->cursor_pos.index, cursor->buffer->context_len);
     }
 }
