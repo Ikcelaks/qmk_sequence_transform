a9a766091f9d6fab52a46c804da753e0ccca4b5f
QKekos <q.kekos.q@gmail.com>
2024-03-21 15:27:32 +0200

    removed ascii -> utf-8 conversion

--- companion/hid_listener/MonitorDevice.py
+++ companion/hid_listener/MonitorDevice.py
@@ -9,10 +9,6 @@ from .observers import Observer
 from .Console import Console
 from .Device import Device
 
-THIS_FOLDER = Path(__file__).parent
-config_file = THIS_FOLDER / "../../../sequence_transform_config.json"
-config = json.load(open(config_file, 'rt', encoding="utf-8"))
-
 
 class MonitorDevice:
     def __init__(self, hid_device: Device, observers: Iterable[Observer] = ()):
@@ -33,7 +29,7 @@ class MonitorDevice:
             self.current_line += self.read(32).replace("\x00", "")
 
         line, self.current_line = self.current_line.split("\n", 1)
-        return self.ascii_to_uft8(line)
+        return line
 
     def read_message(self):
         message = self.read_line()
@@ -48,13 +44,3 @@ class MonitorDevice:
 
             except hid.HIDException:
                 break
-
-    @staticmethod
-    def ascii_to_uft8(line: str) -> str:
-        for token, ascii_char in config["sequence_token_symbols"].items():
-            line = line.replace(ascii_char, token)
-
-        for token, ascii_char in config["wordbreak_symbol"].items():
-            line = line.replace(ascii_char, token)
-
-        return line
